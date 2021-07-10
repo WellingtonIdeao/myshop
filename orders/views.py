8f41621fc0dd42e8fc2019bd3cc750e1cb4bf700
Wellington Ideao <wellington.ideao@gmail.com>
2021-07-10 20:11:12 -0300

    Generating PDF invoices dynamically via weasyPrint

--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,10 @@ from cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import OrderItem, Order
 from django.contrib.admin.views.decorators import staff_member_required
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+import weasyprint
+from myshop.settings import development as settings
 
 
 def order_create(request):
@@ -30,5 +34,18 @@ def order_create(request):
 def admin_order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     return render(request, 'admin/orders/order/detail.html', {'order': order})
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(response,
+                                           stylesheets=[weasyprint.CSS(
+                                               settings.STATIC_ROOT + 'shop/css/pdf.css')])
+    return response
+
 
 
