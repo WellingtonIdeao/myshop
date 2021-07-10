896b5de927ed16d05088365405708226c2820b7d
Wellington Ideao <wellington.ideao@gmail.com>
2021-07-10 17:58:30 -0300

    Add custom view to display information about an order on admin site

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from cart.cart import Cart
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import OrderItem, Order
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 def order_create(request):
@@ -23,5 +24,11 @@ def order_create(request):
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
 
 
