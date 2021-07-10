2ac7462823b5cc88a96f85c5fbf76c5e8cc9c497
Wellington Ideao <wellington.ideao@gmail.com>
2021-07-09 22:31:14 -0300

    asynchronous tasks with celery and monitoring celery with flower

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import OrderItem
+from .tasks import order_created
 
 
 def order_create(request):
@@ -15,8 +16,13 @@ def order_create(request):
                                          price=item['price'], quantity=item['quantity'])
             # clear the cart
             cart.clear()
+
+            # launch asynchronous task
+            order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order})
 
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+
